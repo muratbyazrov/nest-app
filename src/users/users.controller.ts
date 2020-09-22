e51071b904a09cae93ec50120be771eba47fdf76
Murat <murat.byazrov@yandex.ru>
2020-09-22 15:38:47 +0300

    feat: -добавил авторизацию пользователей с созданием токена

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'; // почему здесь надо импортировать body? - потому что все используемые декараторы надо импортировать
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common'; // почему здесь надо импортировать body? - потому что все используемые декараторы надо импортировать
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const data = require('../../data/users.json'); // доступ к users.json
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const bcrypt = require('bcrypt'); //  для хеширования пароля
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs'); // модуль для работы с файлами
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const jwt = require('jsonwebtoken'); // моуль для создания токенов
 
 class CreateUserDto { // это схъема пользователя
   id: number;
@@ -14,6 +16,10 @@ class CreateUserDto { // это схъема пользователя
   password: string;
 }
 
+class LoginUserDto {
+  email: string;
+  password: string;
+}
 
 @Controller('users')
 export class UsersController {
@@ -33,6 +39,27 @@ export class UsersController {
     return 'Регистрация успешна';
   }
 
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    const { email, password } = loginUserDto; // достали body запроса
+
+    const user = await this.findUser(email); //Ждем, пока ведется поиск по email
+    if (user == undefined) { // если такого email нет, сразу выкидываем ошибку
+      return Promise.reject(new Error('Неправильные почта или пароль'));
+    }
+    const { id, name, userEmail, enabled } = user;
+
+    const matched = await bcrypt.compare(password, user.password); // сравнивам хеши паролей
+    if (!matched) { // если сравнение возвращает false, кидаем ошибку
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
+    // если сравнение возвращает true, создаем токен и записываем его в поля пользователя
+    const token = jwt.sign({ id: user.id }, 'token-key', { expiresIn: 3600 * 4 });
+    user.token = token;
+    this.updateArr(user, { id, name, userEmail, enabled, password, token });
+    return `Успешная Авторизация. Ваш токен: ${token}`;
+  }
+
   // записывает массив данных в user.json
   private async updateData() {
     const newArray = JSON.stringify(data);
@@ -41,6 +68,19 @@ export class UsersController {
         return Promise.reject(new Error(err));
       }
     });
+  }
+
+  private updateArr(element, obj) {
+    const token = element.token; // токен оставляем как был
+    const { id, name, email, enabled, password } = obj;
+    const elementIndex = data.indexOf(element);
+    const updateElement = { id, name, email, enabled, password, token };
+    data.splice(elementIndex, 1, updateElement);
+    this.updateData();
+  }
+
+  private async findUser(email) {
+    return Promise.resolve(data.find(item => item.email == email));
   }
 
 }

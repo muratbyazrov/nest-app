0c84a1dba907c21312de2d7ded63d722a74229d2
Murat <murat.byazrov@yandex.ru>
2020-09-24 12:14:14 +0300

    feat: added response timer

--- src/middlewares/timerMiddleware.ts
+++ src/middlewares/timerMiddleware.ts
@@ -1,10 +1,15 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
+const { performance } = require('perf_hooks');
 
 @Injectable()
 export class TimerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function){
-    console.time('Watcher')
+    const start = performance.now();
+    res.on("finish", () => {
+      const resTime = performance.now()-start;
+      console.log(resTime);
+    })
     next()
   }
 }

86ab6a8b62fd0c0ef230768465238fbeb4bc9e7f
Murat <murat.byazrov@yandex.ru>
2020-09-24 11:48:11 +0300

    fix: исправил баг с потерей полей при авторизации

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,7 +14,7 @@ export class UsersService {
   // регистрация
   async createUser(createUserDto): Promise<void> {
     const { name, email, enabled, password } = createUserDto;
-    const id = data.length+1; // задаем последовательный уникальный id
+    const id = data.length + 1; // задаем последовательный уникальный id
     const token = '';
     const hash = await bcrypt.hash(password, 10);
     data.push({ id, name, email, enabled, password: hash, token });
@@ -24,20 +24,19 @@ export class UsersService {
   //авторизация
   async login(loginUserDto) {
     const { email, password } = loginUserDto; // достали body запроса
-    const user = await data.find(item => item.email == email) // Ждем, пока ведется поиск по email
+    const user = await data.find(item => item.email == email); // Ждем, пока ведется поиск по email
     if (user == undefined) { // если такого email нет, сразу выкидываем ошибку
-      return Promise.reject(new Error('Неправильные почта или пароль'));
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     }
-    const { id, name, userEmail, enabled } = user;
 
     const matched = await bcrypt.compare(password, user.password); // сравнивам хеши паролей
     if (!matched) { // если сравнение возвращает false, кидаем ошибку
       throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     }
-    // если сравнение возвращает true, создаем токен и записываем его в поля пользователя
+    // если сравнение возвращает true, создаем токен и записываем его в user
     const token = jwt.sign({ id: user.id }, 'token-key', { expiresIn: 3600 * 4 });
     user.token = token;
-    this.updateArr(user, { id, name, userEmail, enabled, password, token });
+    this.updateArr(user);
   }
 
   async updateUser(createUserDto, params) {
@@ -48,7 +47,16 @@ export class UsersService {
       throw new HttpException('BAD REQUEST. Нет такого пользователя', HttpStatus.BAD_REQUEST);
     }
     const hash = await bcrypt.hash(password, 10); // хешируем измененый пароль
-    this.updateArr(updateElement, { id, name, email, enabled, password:hash }); // id не будем менять
+    const token = jwt.sign({ id }, 'token-key', { expiresIn: 3600 * 4 });
+    this.updateArr({ id, name, email, enabled, password: hash, token }); // id не будем менять
+  }
+
+  // обновить массив данных
+  private async updateArr(obj) {
+    const updatingElement = await data.find(item => item.id == obj.id); // найдем обновляемый элемнт
+    const elementIndex = data.indexOf(updatingElement); // определяем индекс обновляемого элемента
+    data.splice(elementIndex, 1, obj); // начиная с elementIndex удаляем 1 элемент и вставляем obj
+    this.updateData(); // обновляем данные в файле
   }
 
   // обновить файл с данными
@@ -59,15 +67,5 @@ export class UsersService {
         return Promise.reject(new Error(err));
       }
     });
-  }
-
-  // обноваить массив данных
-  private updateArr(element, obj) {
-    const token = element.token; // токен оставляем как был
-    const { id, name, email, enabled, password } = obj;
-    const elementIndex = data.indexOf(element);
-    const updateElement = { id, name, email, enabled, password, token };
-    data.splice(elementIndex, 1, updateElement);
-    this.updateData();
   }
 }
